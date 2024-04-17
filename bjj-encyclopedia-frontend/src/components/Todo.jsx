f0b097e589a1e3f19eb8d5ac93c718cbd95b3a06
robj1925 <robj1925@gmail.com>
2024-04-17 18:45:16 -0400

    everything is fully functioning ready to gooo

--- bjj-encyclopedia-frontend/src/components/Todo.jsx
+++ bjj-encyclopedia-frontend/src/components/Todo.jsx
@@ -2,37 +2,63 @@ import React, { Fragment, useEffect, useState } from "react";
 import TodoItem from "./todoItem";
 
 export function Todo() {
-  const [todoItems, setTodoItems] = useState(null);
+  //this is the "root", kind of like our controller
+  const [todoItems, setTodoItems] = useState(null); //getter and setter
   useEffect(() => {
+    //we use this once the app is loaded
     //do something load
     console.log("Hey, ive loaded up");
     if (!todoItems) {
-      fetch("http://localhost:8081/tasks")
+      //if we dont have anything todo items
+      fetch("http://localhost:8081/tasks") //then we go and fetch them
         .then((response) => response.json())
         .then((data) => {
           console.log(data);
-          setTodoItems(data);
+          setTodoItems(data); //populates view w/ items
         });
     }
   }, [todoItems]);
 
-  //ternary operator
-  // if (something)
-  //    od item 1
-  // else
-  //    do item 2
-  //
-  //condition ? do item1 : do item2
+  function addNewTodoItem() {
+    fetch("http://localhost:8081/addTask", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((response) => response.json()) //take the response obj which is a string in jsonformat and turn it into JSON
+      .then((data) => {
+        setTodoItems([...todoItems, data]); //adds new item added
+        console.log(data);
+      });
+  }
+  function handleDeleteTodoItem(item) {
+    const updateTodoItems = todoItems.filter(
+      (aTodoItem) => aTodoItem.id !== item.id
+    );
+    setTodoItems([...updateTodoItems]);
+  }
   return (
-    <div>
-      {todoItems
-        ? todoItems.map((todoItem) => {
-            return (
-              <TodoItem key={todoItem.id} data={todoItem} />
-            );
-          })
-        : "loading data..."}
-    </div>
+    // we can only return one parent tag
+    <>
+      <div>
+        <button onClick={addNewTodoItem}>Add New Task</button>
+      </div>
+      <div>
+        {todoItems //we are returning an array of todo items that we are fetching from the db
+          ? todoItems.map((todoItem) => {
+              //iterate thru array,
+              return (
+                <TodoItem
+                  key={todoItem.id}
+                  data={todoItem}
+                  emitDeleteTodoItem={handleDeleteTodoItem}
+                />
+              );
+            })
+          : "loading data..."}
+      </div>
+    </>
   );
 }
 
--- bjj-encyclopedia-frontend/src/components/todoItem.jsx
+++ bjj-encyclopedia-frontend/src/components/todoItem.jsx
@@ -1,20 +1,67 @@
 import React, { useEffect, useState } from "react";
 
 const TodoItem = (props) => {
-    const [todoItem, setTodoItem] = useState(props.data);
+  const { emitDeleteTodoItem } = props;
+  const [todoItem, setTodoItem] = useState(props.data);
+  const [isDirty, setDirty] = useState(false);
 
-    useEffect(() => {
-      console.log("Hey, the todo item has changed", todoItem);
-    }, [todoItem]);
-
-    function updateIsDone () {
-        setTodoItem({...todoItem, status: !todoItem.status});
+  useEffect(() => {
+    if (isDirty) {
+      fetch(`http://localhost:8081/tasks/${todoItem.id}`, {
+        method: "PUT",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(todoItem), // have to send in JSON, cant send objects
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setDirty(false);
+          setTodoItem(data);
+        });
     }
+    console.log("Hey, the todo item has changed", todoItem);
+  }, [todoItem, isDirty]); // when the todoitem changes, it calls the useEffect method, which tells our server it changed
+
+  function updateTask(e) {
+    setTodoItem({ ...todoItem, title: e.target.value }); //async
+    setDirty(true);
+  }
+  function deleteTodoItem() {
+    fetch(`http://localhost:8081/tasks/${todoItem.id}`, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(todoItem), // have to send in JSON, cant send objects
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        emitDeleteTodoItem(todoItem);
+      });
+  }
   return (
-    <>
-      <input type="checkbox" checked={todoItem.status} onChange={updateIsDone}/>
-      <span>{todoItem.title}</span>
-    </>
+    <div>
+      <input
+        type="checkbox"
+        checked={todoItem.status}
+        onChange={() => {
+          setDirty(true);
+          setTodoItem({ ...todoItem, status: !todoItem.status });
+        }}
+      />
+      {todoItem.status ? (
+        <span style={{ textDecoration: "line-through" }}>{todoItem.title}</span>
+      ) : (
+        <input type="text" value={todoItem.title} onChange={updateTask} />
+      )}
+      <span
+        style={{ marginLeft: "2rem", cursor: "pointer" }}
+        onClick={deleteTodoItem}
+      >
+        🗑️
+      </span>
+    </div>
   );
 };
 
